7a862ef4ac4bf0c1c063a7d0c7c90fda97833db4
yordan <yordan@gmail.com>
2026-05-29 10:38:43 -0500

    sem10 avance2

--- SE10-P/Cajero.py
+++ SE10-P/Cajero.py
@@ -1,1 +1,20 @@
 class Cajero:
+    saldo = 1000
+
+    def consultar(self)->float:
+        return self.saldo
+
+    def depositar(self, monto)->None:
+        while True:
+            try:
+                m = float(monto)
+                if monto > 0:
+                    self.saldo += m
+                    print("\nDéposito exitoso")
+                    break
+                else:
+                    print("\nNo se puede depositar montos negativos")
+                    break
+            except ValueError:
+                print("\nError: Ingrese solo números válidos!")
+                break
--- SE10-P/SE10_P.py
+++ SE10-P/SE10_P.py
@@ -1,1 +1,20 @@
+from Cajero import Cajero
+c= Cajero()
 
+continuar="si"
+
+while continuar=="si":
+    print("BIENVENIDOS AL SISTEMA DE CAJERO\n")
+    print("1. Consultar saldo")
+    print("2. Depositar dinero")
+    print("3. Retirar dinero")
+    print("4. Salir\n")
+
+    while True:
+        opc = int(input("Ingrese una opción: "))
+
+        if opc in(1,2,3,4): break
+        else: print("Opción no válida. Intente nuevamente.\n")
+
+    continuar = input("\n¿Desea continar? presione [si]: ")
+    
